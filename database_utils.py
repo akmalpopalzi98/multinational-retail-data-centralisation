9efdcaa7207d298fb8bfc44311305bf9b12057bc
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-01-26 17:05:25 +0000

    adding changes

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,34 @@
+#%%
+import yaml
+from yaml.loader import SafeLoader
+from sqlalchemy import create_engine
+
+#%%
 class DatabaseConnector():
-    pass
+    def __init__(self,file_name):
+        self.file_name = file_name
+
+    def read_db_creds(self):
+        with open(self.file_name) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            return data
+
+    def init_db_engine(self):
+        data_2 = self.read_db_creds()
+        engine = create_engine(f"postgresql+psycopg2://{data_2['RDS_USER']}:{data_2['RDS_PASSWORD']}@{data_2['RDS_HOST']}:{data_2['RDS_PORT']}/{data_2['RDS_DATABASE']}")
+        if engine.connect():
+            print('Connection Sucessfull')
+        return engine
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
