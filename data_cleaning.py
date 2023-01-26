6a15680ca28456c0981d5ec8c3cffd74c99554a2
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-01-26 21:00:44 +0000

    adding method

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,2 +1,27 @@
+import data_extraction,database_utils
+from data_extraction import DataExtractor
+from database_utils import DatabaseConnector
+import pandas as pd
+from dateutil import parser
+#%%
+def has_numbers(inputString):
+    return any(char.isdigit() for char in inputString)
+#%%
 class DataCleaning():
-    pass
+    def __init__(self,dataframe):
+        self.dataframe = dataframe
+
+
+    def clean_user_data(self):
+        clean_1_no_null = self.dataframe[self.dataframe.country_code != 'NULL']
+        messy_first_names = []
+        for item in clean_1_no_null.first_name:
+            if has_numbers(item):
+                messy_first_names.append(item)
+        clean_2_users = clean_1_no_null[~clean_1_no_null.first_name.isin(messy_first_names)]
+        clean_3_country_code = clean_2_users.replace('GGB','GB')
+
+                
+
+
+
